4768fbd3b14150d435fd6f1be40f7541dd33cd2c
23489ac <23489ac@hvhs.school.nz>
2025-04-30 00:11:26 +0000

    Apr 30, 2025, 12:11 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,11 +7,11 @@ The user name
 The user age 
 How much pocket money the user has.
 The current year.
+console.log(user) 
 Hi _______
-
 As of ____ you are __ years old
-
 You have __ dollars
+
 /***************************
  Main code
 ***************************/
