d63e253a3e1ea66bc51b72112bf1ef079bc7a92a
23489ac <23489ac@hvhs.school.nz>
2025-05-09 02:21:38 +0000

    May 9, 2025, 2:21 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,9 +4,19 @@
  in term 2
 
 ****************************/
-console.log(math);
-
- //Varibles
+//Varibles
+ var num1 =10
+ var num2 =5
+var answer;
+ //console.log
+answer = num1 + num2;
+console.log("add",     answer);
+answer = num1 - num2;
+console.log("subtract",     answer);
+answer = num1 * num2;
+console.log("multiply",     answer);
+answer = num1 / num2;
+console.log("divide",     answer);
 
 
 /***************************
