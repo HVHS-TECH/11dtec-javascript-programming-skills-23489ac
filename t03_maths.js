c8a6148c90c6084c185847f370f674a8ad2bfb36
23489ac <23489ac@hvhs.school.nz>
2025-05-12 20:39:15 +0000

    May 12, 2025, 8:39 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,31 +1,7 @@
-/****************************
- intro 1
- written by Aarti
- in term 2
+let num1 = 10;
+let num2 = 5;
 
-****************************/
-//Varibles
- var num1 =10
- var num2 =5
-var answer;
- //console.log
-answer = num1 + num2;
-console.log("add",     answer);
-answer = num1 - num2;
-console.log("subtract",     answer);
-answer = num1 * num2;
-console.log("multiply",     answer);
-answer = num1 / num2;
-console.log("divide",     answer);
-
-
-/***************************
- Main code
-***************************/
-
-
-
-
-/***************************
-Funcations
-***************************/
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
