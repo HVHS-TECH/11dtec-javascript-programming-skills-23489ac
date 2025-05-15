19e6931c8f885064d69da6593875774768e27621
23489ac <23489ac@hvhs.school.nz>
2025-05-15 00:59:02 +0000

    May 15, 2025, 12:59 AM

--- t09_functions.js
+++ t09_functions.js
@@ -4,11 +4,30 @@
 console.log("runing T1-intro.js");
 
 //Variables
+function getName() {
+  return prompt("What is your name?");
+}
+
+function getAge() {
+  return Number(prompt("How old are you?"));
+}
 
 
 /******************************
  Main codecode
 ******************************/
+function checkMoney() {
+  let money = Number(prompt("How much pocket money do you have?"));
+  if (money >= 4) {
+    alert("You CAN buy a chocolate bar!");
+  } else {
+    alert("You CAN'T buy a chocolate bar.");
+  }
+}
+
+name = getName();
+age = getAge();
+ch
 
 
 /******************************
