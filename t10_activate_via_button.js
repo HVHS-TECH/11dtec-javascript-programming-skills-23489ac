bbff544f901cf68cf787c330a68fcde2cc448565
23489ac <23489ac@hvhs.school.nz>
2025-05-15 01:11:24 +0000

    May 15, 2025, 1:11 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -11,8 +11,14 @@ let usermoney;
 /******************************
  Main codecode
 ******************************/
-
-
+//stat()
+//run the program
+//called by the stat button
 /******************************
+ funcation start()  {
+ username = askname();
+ userage = askuserage();
+ usermoney = askmoney();
+ {
  Functions
 ******************************/
