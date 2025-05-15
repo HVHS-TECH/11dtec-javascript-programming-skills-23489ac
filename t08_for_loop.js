77f4e7b52cc659921a99b5ccec8fb66291b109fb
23489ac <23489ac@hvhs.school.nz>
2025-05-15 00:54:45 +0000

    May 15, 2025, 12:54 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -4,11 +4,20 @@
 console.log("runing T1-intro.js");
 
 //Variables
+let total = 0;
+
 
 
 /******************************
  Main codecode
 ******************************/
+for (let i = 0; i < 5; i++) {
+  let number = Number(prompt("Enter number " + (i + 1)));
+  total = total + number;
+}
+
+alert("The total is: " + total);
+
 
 
 /******************************
