3e235d7e19493acdb86b1f401d0f5cafc1f54350
23489ac <23489ac@hvhs.school.nz>
2025-05-15 00:47:24 +0000

    May 15, 2025, 12:47 AM

--- t07_if.js
+++ t07_if.js
@@ -4,6 +4,9 @@
 console.log("runing T1-intro.js");
 
 //Variables
+name = prompt("What is your name?");
+age = Number(prompt("How old are you?"));
+money = Number(prompt("How much pocket money do you have?"));
 
 
 /******************************
