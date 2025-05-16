9b075be3e1cd8825be91c6ab3b1217edf106ee7c
23489ac <23489ac@hvhs.school.nz>
2025-05-16 02:38:05 +0000

    May 16, 2025, 2:38 AM

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -4,12 +4,17 @@
 console.log("runing T1-intro.js");
 
 //Variables
-
+UserAge
 
 /******************************
  Main codecode
 ******************************/
-
+userage = prompt("how old ar you?");
+  if(userAge <=5   || userAge <=65){
+    arlet('you get a disscounted on choclate');
+  }else{
+    arlet('sorry, you must pay the full price');
+  }
 
 /******************************
  Functions
