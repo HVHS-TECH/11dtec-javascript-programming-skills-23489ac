8dfb58984001776b05ec0d9f5bf6b4fa94fbc3d0
23489ac <23489ac@hvhs.school.nz>
2025-05-26 21:21:03 +0000

    May 26, 2025, 9:21 PM

--- t13_validation.js
+++ t13_validation.js
@@ -4,8 +4,16 @@
 console.log("runing T1-intro.js");
 
 //Variables
-const 
+const DRIVINGE =16;
+var userAge;
 
+userAge = prompt("what is your age");
+if(UserAge >=DRIVINGE) {
+    alert ("your are old enough to drive");
+}
+else{
+    alert("YOu are not old enoung to drive");
+}
 /******************************
  Main codecode
 ******************************/
