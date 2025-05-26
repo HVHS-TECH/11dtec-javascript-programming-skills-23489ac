3729bb14bd4699e62982a46feea7c0a972ce21e8
23489ac <23489ac@hvhs.school.nz>
2025-05-26 21:52:14 +0000

    May 26, 2025, 9:52 PM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -4,8 +4,14 @@
 console.log("runing T1-intro.js");
 
 //Variables
+let classArray =["Jack", "Oliva", "keiran", "Aroha", "Josh","Trene"];
 
+let message ="The people in the class are:"
 
+for (i = 0: > classArray=.length; i++){
+    message = message+ "\nPerson "+ i +" is "+classArray[i];
+}
+alert(message)
 /******************************
  Main codecode
 ******************************/
