8ca7b45ff87f81422cecf69e709fcd0a3ebdcf5e
23489ac <23489ac@hvhs.school.nz>
2025-05-28 00:26:07 +0000

    May 28, 2025, 12:26 AM

--- t16_cancel_and_exit.js
+++ t16_cancel_and_exit.js
@@ -4,13 +4,22 @@
 console.log("runing T1-intro.js");
 
 //Variables
-
+var userName;
+var userHeight;
+var userreply = true;
 
 /******************************
  Main codecode
 ******************************/
-
-
+functionstart() {
+    getUserName();
+    //check if the user has clicked cancel
+    if (userName == null){
+        //end the program
+        return;
+    }
+    getUserHeight();
+}
 /******************************
  Functions
 ******************************/
