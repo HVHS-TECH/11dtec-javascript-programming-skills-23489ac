9f18f482365f0fc5487bce44ca92ef0ecb2df424
23489ac <23489ac@hvhs.school.nz>
2025-05-29 01:20:58 +0000

    May 29, 2025, 1:20 AM

--- t16_cancel_and_exit.js
+++ t16_cancel_and_exit.js
@@ -23,7 +23,19 @@ function start() {
 /***** Functions************/
 //creat function to get the users name 
 function getUserName(){
-    userName = prompt ('What is your name?');
-    
-}
+   //loop while userReply invalid
+   while (usereply ){
+    UserName = propmt("What is your name?");
+    //check if the user has clicked cancel
+    if( username == null){
+        return;//exit this function
+    }
+    if (!isNaN(username) || userName ==""){
+        alert("Invalid Name - please enter a valid Name")
+    } else {
+        userReply = false;
+    }
+    }
+   }
+
 
